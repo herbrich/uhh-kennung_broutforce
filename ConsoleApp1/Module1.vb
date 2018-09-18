8f5533fc799458d625ff7c49d9f82f2f19013aa3
Aleksandar J. Herbrich <jenniih@herbrich.her>
2018-09-19 01:59:22 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.ForegroundColor = ConsoleColor.Green
+        HackUniHamburg()
+    End Sub
+    Sub HackUniHamburg()
+        For id As Integer = 1000 To 9999 Step 1
+            Dim kennung = "bxy" & CStr(id)
+            WriteUniHamburgLogin(kennung)
+            Console.WriteLine(kennung)
+        Next
+    End Sub
+    Sub WriteUniHamburgLogin(ByVal unikennung As String)
+        If (unikennung.Length = 7) Then
+            My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath & "\uni_logins.tx", unikennung & Environment.NewLine, True)
+        End If
+    End Sub
+End Module
---
+++ ConsoleApp1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("ConsoleApp1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleApp1")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("12dcbbee-f0b8-4920-a172-56131a3df62f")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
